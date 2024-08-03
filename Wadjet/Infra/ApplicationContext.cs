aa47d9fb38f9f7701c653e7faf19083fa9146d79
Fabiano Teodoro <faateodoro@gmail.com>
2024-08-02 22:44:45 -0300

    Add Infra

---
+++ Wadjet/Infra/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wadjet.Entities;
+
+namespace Wadjet.Infra;
+
+public class ApplicationContext : DbContext
+{
+
+    public DbSet<Establishment> Establishments { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+
+    }
+}
