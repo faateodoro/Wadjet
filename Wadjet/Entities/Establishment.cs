240f58967818ac1640d8c9cf360b03465bc4a81a
Fabiano Teodoro <faateodoro@gmail.com>
2024-08-02 22:59:46 -0300

    Add basic infos to Establishment class

--- Wadjet/Entities/Establishment.cs
+++ Wadjet/Entities/Establishment.cs
@@ -2,12 +2,25 @@
 
 public class Establishment
 {
+    public long Id { get; private set; }
     public string Name { get; private set; }
     public string CNPJ { get; private set; }
     public string Address { get; private set; }
     public string Phone { get; private set; }
     public int MotorcycleVacancy { get; private set; }
     public int CarVacancy { get; private set; }
-    public DateTime CreatedAt { get; private set; }
+    public DateTime CreatedAt { get; private set; } = DateTime.Now;
 
+    [Obsolete]
+    protected Establishment() { }
+
+    public Establishment(string name, string cNPJ, string address, string phone, int motorcycleVacancy, int carVacancy)
+    {
+        Name = name;
+        CNPJ = cNPJ;
+        Address = address;
+        Phone = phone;
+        MotorcycleVacancy = motorcycleVacancy;
+        CarVacancy = carVacancy;
+    }
 }
