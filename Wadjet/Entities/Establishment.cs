8ad7d62aea1328bef966a14cc61b03e20b0bf05d
Fabiano Teodoro <faateodoro@gmail.com>
2024-08-02 22:22:05 -0300

    Add Establishment

---
+++ Wadjet/Entities/Establishment.cs
@@ -0,0 +1,13 @@
+﻿namespace Wadjet.Entities;
+
+public class Establishment
+{
+    public string Name { get; private set; }
+    public string CNPJ { get; private set; }
+    public string Address { get; private set; }
+    public string Phone { get; private set; }
+    public int MotorcycleVacancy { get; private set; }
+    public int CarVacancy { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+}
